fd02585d5c6ec6dfacdf443c98aafb93b5c23766
dekeyel_laptop <dekeyel_laptop@gmail.com>
2019-11-03 17:44:38 +0300

    refactoring code and add create ved

--- parser/views.py
+++ parser/views.py
@@ -1,110 +1,52 @@
 import requests
+from bs4 import BeautifulSoup
 import sqlite3
 
-#from echo.config import HOME_DIR
-
-def create_db():
-    #CREATE TABLE students(id INTEGER PRIMARY KEY AUTOINCREMENT, n_zach INTEGER);
-    """CREATE TABLE subjects(
-    id INTEGER PRIMARY KEY AUTOINCREMENT ,
-    name VARCHAR(50),
-    id_site INTEGER
-    );"""
-    """#CREATE TABLE rating(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_student INTEGER,
-    id_subject INTEGER,
-    lec_1 VARCHAR(10),
-    pr_1 VARCHAR(10),
-    lab_1 VARCHAR(10),
-    skip_1 VARCHAR(10),
-    result_1 VARCHAR(10),
-    lec_2 VARCHAR(10),
-    pr_2 VARCHAR(10),
-    lab_2 VARCHAR(10),
-    skip_2 VARCHAR(10),
-    result_2 VARCHAR(10),
-    exam VARCHAR(10),
-    all_point VARCHAR(10),
-    oc VARCHAR(10),
-    result_oc VARCHAR(10),
-    FOREIGN KEY(id_student) REFERENCES students(id),
-    FOREIGN KEY(id_subject) REFERENCES subjects(id)
-    );"""
-    pass
-
-def insert_student(n_zach):
-    conn = sqlite3.connect("{}/main.db".format(HOME_DIR))
-    cursor = conn.cursor()
-
-    cursor.execute("INSERT INTO students(n_zach) VALUES(?)", (n_zach, ))
-
-    conn.commit()
-
-def insert_subject(name, id_site):
-    conn = sqlite3.connect("../main.db")
-    cursor = conn.cursor()
-
-    cursor.execute("INSERT INTO subjects(name, id_site) VALUES(?, ?)", (name, id_site))
-
-    conn.commit()
-
-def insert_rating(id_student, id_subject):
-    pass
-
-def take_rating(student ,lines):
-    return tuple(take_results(
-                       lines[lines.find('ctl00_MainContent_ucVedBox_TableVed_DXDataRow{}'.format(find_position_in_site(str(student), lines) - 1))::].split('">')[2:16]
-                      ))
-
-def take_results(string):
-    mass = []
-    for i in string:
-        znach = i.split('<')[0]
-        if znach == '&nbsp;':
-            mass.append('')
-        else:
-            mass.append(znach)
-    return mass
-
-def create_message(arg, mass, subject_name):
+def create_message(mass, arg, subject_name):
     res = []
     for i in range(len(arg)):
         if mass[i] != '':
-            res.append('{} {}'.format(arg[i], mass[i]))
+            res.append('{}: {}'.format(arg[i], mass[i]))
 
     message = subject_name
     for i in res:
         message = '{}\n{}'.format(message, i)
 
-    return res
+    return message
 
-def find_position_in_site(id_book, lines):
-    try:
-        return int(lines[:lines.find(id_book)].split('<td class=')[-2].split('</td>')[0].split('>')[1])
-    except:
-        return False
+def get_position(soup:BeautifulSoup, n_zach):
+    soup = soup.find_all('td', 'dx-al')
+    for i in range(len(soup)):
+        if soup[i].text == n_zach:
+            return i
 
+def get_rating(soup:BeautifulSoup, n_zach):
+    soup = soup.find('tr', id='ctl00_MainContent_ucVedBox_TableVed_DXDataRow{}'.format(get_position(soup, n_zach))).find_all('td')
+    mass = []
 
-if __name__ == '__main__':
-    '''peyload = {'id': 687891}
-    url = 'https://edu.donstu.ru/Ved/Ved.aspx'
+    for i in soup:
+        if i.text == '&nbsp;' or i.text == '\xa0':
+            mass.append('')
+        else:
+            mass.append(i.text)
 
-    r = requests.get(url, params=peyload)
-    lines = r.text
-    num = find_position_in_site('1795989', lines)
-    print(take_results(
-                       lines[lines.find('ctl00_MainContent_ucVedBox_TableVed_DXDataRow{}'.format(num - 1))::].split('">')[2:16]
-                      )
-         )
-    '''
-    a = {
-        'Анализ и кодирование информации': 687891,
-        'Базы данных': 687892,
-        'Объектно-ориентированное программирование': 687894,
-        'Операционные системы': 687896,
-        'Программирование под платформу .NET': 687897,
-        'Сети и телекоммуникации': 687898
-    }
-    for i in a:
-        insert_subject(i, a[i])
+    return tuple(mass)
+
+def get_ved(group):
+    url = 'https://edu.donstu.ru/Ved/'
+    user_agent = ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) '
+                  'Gecko/20100101 Firefox/50.0')
+
+    r = requests.get(url, params={'group':group}, headers={'User-Agent': user_agent})
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    soup = soup.find_all('tr', 'dxgvDataRow_MaterialCompact')
+
+    mass = []
+
+    for i in soup:
+        mass.append((i.find_all('td')[0].text + ' ' + i.find_all('td')[1].text,
+                     str(i.find_all('td')[0].find_all('a')[0]).split('=')[-1].split('"')[0]))
+
+    return mass
