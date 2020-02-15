2a0df977d908df137ed379014da243c61d85a829
dekeyel <dekeyel>
2020-02-15 21:28:16 +0300

    fix exeptions DB

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from parser.views import *
+from pymysql import p
 
 from datetime import datetime
 from time import sleep
@@ -27,19 +28,24 @@ while True:
             if r.status_code == 200:
                 soup = BeautifulSoup(r.text, 'html.parser')
 
-                for j in students:
-                    rating_old = DB.take_rating(j, i)
-                    rating_new = get_rating(soup, j)
+                try:
+                    for j in students:
+                        rating_old = DB.take_rating(j, i)
+                        rating_new = get_rating(soup, j)
 
-                    if rating_new != rating_old[:len(rating_new)] and len(rating_new) > 2:
-                        DB.update_student_rating(j, i, rating_new)
-                        subject_name = DB.take_subject_name(i)
-                        message = create_message(rating_new, subject_name, DB.take_name(j), DB.take_subjects_args(i))
-                        if message:
-                            send_message_chat(message)
-                        print(transliterate.translit(subject_name, reversed=True),
-                              j, datetime.today().strftime("%Y-%m-%d %H.%M.%S")
-                              )
+                        if rating_new != rating_old[:len(rating_new)] and len(rating_new) > 2:
+                            DB.update_student_rating(j, i, rating_new)
+                            subject_name = DB.take_subject_name(i)
+                            message = create_message(rating_new, subject_name, DB.take_name(j),
+                                                     DB.take_subjects_args(i))
+                            if message:
+                                send_message_chat(message)
+                            print(transliterate.translit(subject_name, reversed=True),
+                                  j, datetime.today().strftime("%Y-%m-%d %H.%M.%S")
+                                  )
+                except:
+                    print('BD drop')
+                    DB = DB()
 
         sleep(10)
 
@@ -52,5 +58,9 @@ while True:
         sleep(120)
 
     except requests.exceptions.Timeout:
+        print('Error at', datetime.today().strftime("%Y-%m-%d %H.%M.%S"), 'Timeout')
+        sleep(120)
+
+    except :
         print('Error at', datetime.today().strftime("%Y-%m-%d %H.%M.%S"), 'Timeout')
         sleep(120)
