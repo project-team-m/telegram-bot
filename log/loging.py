8f710f6343dc731c90a0175d7f1d1f2912347f23
dekeyel <dekeyel>
2020-04-10 12:51:35 +0300

    fix spases log

--- log/loging.py
+++ log/loging.py
@@ -13,13 +13,13 @@ class Log:
     def write_actions_log(self, action):
         self.write_log(action)
         with open(self.action_log, 'a') as log:
-            log.write('\n{}| Time at: {}\n'.format(str(action), datetime.now()))
+            log.write('{}| Time at: {}\n'.format(str(action), datetime.now()))
 
     def write_error_log(self, error):
         self.write_log(error)
         with open(self.error_log, 'a') as log:
-            log.write('\n{}| Error at: {}\n'.format(str(error), datetime.now()))
+            log.write('{}| Error at: {}\n'.format(str(error), datetime.now()))
 
     def write_log(self, string):
         with open(self.log, 'a') as log:
-            log.write('\n{}| Time at: {}\n'.format(str(string), datetime.now()))
+            log.write('{}| Time at: {}\n'.format(str(string), datetime.now()))
